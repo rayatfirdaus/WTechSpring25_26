4dd9b96284e15e6cac0df2b39089c22b637fd6b0
Rayat <rayatsarkar@gmail.com>
2026-03-08 03:18:40 +0600

    JS DOM and Events

--- Lab4/Javascript/Script.js
+++ Lab4/Javascript/Script.js
@@ -1,12 +1,31 @@
+console.log("Script.js is running");
+var a = ["ABC", "DEF"];
+
+let clickcount=0;
+a.forEach((item, index)=>{
+    console.log("Index: ",index, "Item:", item);
+})
+a.map((item, index)=>{
+    console.log("Index: ",index, "Item:", item);
+})
+
 console.log("Connected to Script.js");
 function collect_name() {
     let name = document.getElementById("PatientName").value;
-    console.log("Name collected: " , name);
+    console.log("Name: " , name);
     document.getElementById("PatientName").style.color = "blue";
     return false;
 }
 function collect_address() {
     let address = document.getElementById("Address").value;
     console.log(address);
+    return false;
+}
+//let clickcount=0;
+function submit_key(){
+    clickcount++;
+    let submitdata = document.getElementById("submitbtn").value;
+    document.getElementById("submitbtn").style.color = "blue";
+    document.getElementById("submitbtn").innerHTML="Again Submit" + clickcount;
     return false;
 }
